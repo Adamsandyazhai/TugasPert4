cf459e397e774a217d571de2cf4f962474e5651c
AdamSandyAzhari <147610568+Adamsandyazhai@users.noreply.github.com>
2023-10-18 21:05:07 +0700

    TugasPert4

---
+++ MatematikaBeraksi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author adams
+ */
+public class MatematikaBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+    }
+    public static void main(String[] args) {
+        Matematika matematika = new Matematika();
+
+        int hasilPertambahan = matematika.pertambahan(20, 10);
+        int hasilPengurangan = matematika.pengurangan(10, 5);
+        int hasilPerkalian = matematika.perkalian(10, 3);
+        double hasilPembagian = matematika.pembagian(21, 2);
+
+        System.out.println("Pertambahan: " + 20 + " + " + 10 + " = " + hasilPertambahan);
+        System.out.println("Pengurangan: " + 10 + " - " + 5 + " = " + hasilPengurangan);
+        System.out.println("Perkalian: " + 10 + " * " + 3 + " = " + hasilPerkalian);
+        System.out.println("Pembagian: " + 21 + " / " + 2 + " = " + hasilPembagian);
+    }
+}
